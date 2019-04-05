065649aaada16f4d2c8d25f5562cd96be4f9e080
David Boddie <david.boddie@puri.sm>
2019-04-05 17:19:09 +0200

    Add most of the first tutorial
    Signed-off-by: David Boddie <david.boddie@puri.sm>

---
+++ build-aux/meson/postinstall.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = path.join(prefix, 'share')
+destdir = environ.get('DESTDIR', '')
+
+# Package managers set this so we don't need to run
+if not destdir:
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
---
+++ src/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018 Purism SPC
+# SPDX-License-Identifier: GPL-3.0+
+# Author: David Boddie <david.boddie@puri.sm>
+
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import GLib, Gtk
+
+
+class Application(Gtk.Application):
+
+    def __init__(self):
+        super().__init__(application_id='com.example.first_application')
+        GLib.set_application_name('Your First Application')
+
+    def do_activate(self):
+        window = Gtk.ApplicationWindow(application=self)
+        window.set_icon_name('com.example.first_application')
+
+        label = Gtk.Label()
+        label.set_markup('<span font="40">Hello World!</span>')
+        window.add(label)
+        window.show_all()
+
+
+if __name__ == "__main__":
+
+    app = Application()
+    result = app.run(sys.argv)
+    sys.exit(result)
