5ee4121fcc03fb2ac7bff3888c5d9292a346750d
Page Asgardius <asgardius@asgardius.company>
2024-01-18 08:33:20 -0700

    working

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # asteroid-game-launcher
 
+<img src=https://git.asgardius.company/asgardius/asteroid-game-launcher/raw/branch/master/data/asgardius.page.asteroidlauncher.png>
+
 A launcher for a space themed game
+
+# Dependencies
+
+* python
+* GTK 3
+* VTE 3
+
+# Build dependencies
+* Meson
+* Ninja
+
+# Install instructions
+To build this app
+meson . _build
+To install
+sudo ninja -C _build install
+To uninstall
+sudo ninja -C _build uninstall
--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@
 
 import sys
 import gi
+import subprocess
 gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gtk
+from gi.repository import GLib, Gtk, Vte
+import os
 
 
 class Application(Gtk.Application):
@@ -22,10 +24,42 @@ class Application(Gtk.Application):
         window = Gtk.ApplicationWindow(application=self)
         window.set_icon_name('asgardius.page.asteroidlauncher')
 
+        hbox = Gtk.VBox(spacing=6)
+        window.add(hbox)
+
         label = Gtk.Label()
-        label.set_markup('<span font="40">Hello World!</span>')
-        window.add(label)
+        label.set_markup('<span font="12">Broadcasting with 100000 power watts\nfrom the asteroid belt</span>')
+        hbox.pack_start(label, True, True, 0)
+
+        button = Gtk.Button.new_with_label("Install / Update")
+        button.connect("clicked", self.on_install_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_label("Play")
+        button.connect("clicked", self.on_play_clicked)
+        hbox.pack_start(button, True, True, 0)
+        #window.add(label)
         window.show_all()
+
+    def on_install_clicked(self, button):
+        home = os.path.expanduser("~")
+        terminal     = Vte.Terminal()
+        terminal.spawn_sync(
+            Vte.PtyFlags.DEFAULT,
+            None,
+            ['/bin/bash', '-c', ' wget https://lily.asgardius.company/r3gamedlx64 -O '+home+'/.r3game && chmod +x '+home+'/.r3game && echo \"Game Installed\"'],
+            None,
+            GLib.SpawnFlags.DEFAULT,
+        )
+
+        win = Gtk.Window()
+        win.connect('delete-event', Gtk.main_quit)
+        win.add(terminal)
+        win.show_all()
+
+    def on_play_clicked(self, button):
+        home = os.path.expanduser("~")
+        subprocess.run([home+'/.r3game'])
 
 
 if __name__ == "__main__":
